b894d6779ffcfed554ebdef5f2f8eb49473467a3
Ivan Nikolsky <enty8080@gmail.com>
2021-08-16 21:30:24 +0200

    init 1

--- hatboard/core/static/js/datatable.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', event => {
-    const datatablesSimple = document.getElementById('datatablesSimple');
-    if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple);
-    }
-});
-
