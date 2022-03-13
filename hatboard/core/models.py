90f12c1e6359eca99a97b2d2de21343e0f204753
Ivan Nikolsky <enty8080@gmail.com>
2022-03-14 00:27:21 +0300

    Update models.py

--- hatboard/core/models.py
+++ hatboard/core/models.py
@@ -74,3 +74,10 @@ class Option(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class API(models.Model):
+    token = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.token
